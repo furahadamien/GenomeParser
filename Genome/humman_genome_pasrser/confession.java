ea9c84dc079bf55b8c7bca0ceaabcb2aa641ffd6
Furaha Damien <31530099+furahadamien@users.noreply.github.com>
2018-04-16 23:01:41 -0400

    Update confession.java

--- Genome/humman_genome_pasrser/confession.java
+++ Genome/humman_genome_pasrser/confession.java
@@ -4,6 +4,6 @@ public class confession {
 	/*my code for is Gene works perfectly but when you insert a gene sequence that is not a multiple of 3 it still retains true
 	 * however  the helper method that i developed to check if a sequence is a multiple of 3 works well.
 	 * i couldn't figure out what error i made in calling the helper method.
-	 * this behavior affects the the find gene method since i call isGene to help me validate is a string is indeed a gene. otherwise the rest works fine i believe*/
+	 * this behavior affects the the find gene method since i call isGene to help me validate if a string is indeed a gene. otherwise the rest works fine i believe*/
 
 }
